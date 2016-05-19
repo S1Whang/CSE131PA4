ddc9412b73e89a1ab7417f192cdfaf23962e720f
Samuel Whang <s1whang@ucsd.edu>
2016-05-19 15:42:42 -0700

    Created Symtable

--- symtable.cc
+++ symtable.cc
@@ -3,3 +3,38 @@
  *
  */
 
+#include <sstream>
+#include <iostream>
+#include "symtable.h"
+#include <string>
+Symtable::Symtable() : current(0) {
+  list = vector<map<string,llvm::Value*> >();
+  list.push_back(map<string,llvm::Value*>());
+}
+Symtable::~Symtable() {}
+
+bool
+Symtable::Lookup(string name) {
+  map<string,llvm::Value*>::iterator it;
+  it = list.at(current).find(name);
+  if (it != list.at(current).end())
+    return true;
+  return false;
+}
+
+void
+Symtable::Insert(string name, llvm::Value* val) {
+  list.at(current).insert(pair<string,llvm::Value*>(name,val));
+}
+
+void
+Symtable::Push() {
+  list.push_back(map<string,llvm::Value*>());
+  current++;
+}
+
+void
+Symtable::Pop() {
+  list.pop_back();
+  current--;
+}
--- symtable.h
+++ symtable.h
@@ -3,4 +3,34 @@
  * ----------- 
  *  Header file for Symbol table implementation.
  */
+#ifndef _SYMTABLE_H_
+#define _SYMTABLE_H_
 
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <list>
+#include "irgen.h"
+#include "ast_type.h"
+#include "ast_decl.h"
+using namespace std;
+class Symtable {
+public:
+  Symtable();
+  ~Symtable();
+  bool Lookup(string);
+  void Insert(string,llvm::Value*);
+  void Push();
+  void Pop();
+  int GetCurrent() {return current;}
+  vector<map<string,llvm::Value*> >& GetVector() {return list;}
+  map<string,llvm::Value*>& GetMap() {return list.at(current);}
+  map<string,llvm::Value*>& GetGlobalMap() {return list.at(0);}
+  map<string,llvm::Value*>& GetIndexMap(int i) {return list.at(i);}
+  map<string,llvm::Value*>& GetPrevMap() {return list.at(current-1);}
+protected:
+  vector<map<string,llvm::Value*> >list;
+  int current;
+};
+#endif // _SYMTABLE_H_
